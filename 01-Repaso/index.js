0597113a8b01d7bedbbcafa9f02a08c156e02d0c
tomasjimenez <tomas.jimenez.flores@gmail.com>
2020-06-24 22:00:19 -0500

    aprendiendo a hacer peticiones en local

--- 01-Repaso/index.js
+++ 01-Repaso/index.js
@@ -6,8 +6,20 @@ const request = require('request');
 // const API_URL = ''
 // request.get(API_URL, () => {});
 
-const BR_BA_QUOTES = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
-request.get(BR_BA_QUOTES, (err, res, body) => {
-  if (res.statusCode === 200) console.log(body);
-  else console.log(err);
+// const BR_BA_QUOTES = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+// request.get(BR_BA_QUOTES, (err, res, body) => {
+//   if (res.statusCode === 200) console.log(body);
+//   else console.log(err);
+// });
+
+const getBandInfo = (band) => { 
+const AUDIO_DB = 'theaudiodb.com/api/v1/json/1/search.php?s=coldplay';
+request.get(AUDIO_DB, (err, res, body) => {
+  if (err) console.log(err);
+  if (res.statusCode === 200) {
+      const json == JSON.parse(body);
+      console.log(json.artists[0].strArtist);
+  } 
+  else console.log(res.statusCode, err);
 });
+}
---
+++ 02-API/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const request = require('request');
+
+app.get('/', (req, res) => res.send('Hola Mundo!'));
+
+// app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`)); 
+
+
+
+
+
+// 1.- Agrega un endpoint ‘/api/’ que responda a 
+//         una petición de tipo GET con un código de estado 200 
+//         y el siguiente json: 
+app.get('/api/', (request, response) => {
+    response.status(222).json({ mensaje: 'sup'});
+})    
+
+//     2.- Agrega un endpoint ‘/api/suma’ que responda a una 
+//         petición de tipo GET con la suma de dos números que 
+//         reciba mediante las querys num1 y num2. El servidor
+//         debe responder con un código de estado 200 y un json 
+//         como el siguiente:
+//                         {‘resultado’: 7}
+
+
+
+app.get('/api/suma', (request, response) => {
+    const {num1, num2} = request.query;
+    const resultado = parseInt(num1) + parseInt(num2);
+    response.status(200).json({ resultado: resultado  });
+})  
+
+
+//     3.- Agrega un endpoint ‘/api/usuario/’ que responda a
+//         una petición de tipo GET con el nombre que sea 
+//         recibido a través de params. El servidor debe responder
+//         con un código de estado 200 y un json como este:
+//                       {‘usuario’: ‘Edwin’}
+
+app.get('/api/usuario/:nombreUsuario', (request, response) => {
+    const nombreUsuario = request.params;
+    response.status(200).json({ usuario: nombreUsuario });
+})  
+
+//     4.- Agrega un endpoint ‘/api/swapi’ que responda a una
+//         petición de tipo GET con el personaje solicitado de 
+//                         https://swapi.co/
+//         El cliente debe mandar el número de personaje mediante
+//         params. La respuesta del servidor debe lucir algo así
+//                     {‘personaje’: {
+//                         ‘name’: ‘Luke Skywalker’,
+//                         ...,
+//                     }}
+
+app.get('/api/swapi/:numeroPersonaje', (request, response) => {
+    const numeroPersonaje = request.params;
+    const SWAPI_URL = 'https://swapi.co/';
+    request.get(SWAPI_URL, (err, resp, body) => {
+        if (response.statusCode === 200) console.log(body);
+        else console.log(err);
+    });
+
+    response.status(200).json({ name: numeroPersonaje });
+})  
+
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
+
+// const API_URL = ''
+// request.get(API_URL, () => {});
+
+// const BR_BA_QUOTES = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+// request.get(BR_BA_QUOTES, (err, res, body) => {
+//   if (res.statusCode === 200) console.log(body);
+//   else console.log(err);
+// });
